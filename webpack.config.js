e940c96865c8d6e8f11c6b8b36a4b4011a992e0a
fenndev <fenndev@protonmail.com>
2022-04-24 18:43:09 -0700

    Added source maps and fixed webpack config name

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     mode: 'development',
 
-    entry: '.src/index.js',
+    entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
@@ -18,7 +18,7 @@ module.exports = {
                     loader: 'babel-loader'
                 }
             }
-            
         ]
-    }
+    },
+    devtool: 'source-map'
 }
